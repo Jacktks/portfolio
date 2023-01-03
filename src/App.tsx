61f636057b526eec8d60795ffa1ca96739ec33b7
Tri Nguyen <tri.nguyen@paradox.ai>
2023-01-03 16:25:52 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.scss';
+
+function App() {
+    return (
+        <div
+            className="App"
+            style={{
+                background: '#DBDBDB',
+                minWidth: '100vw',
+                minHeight: '100vh',
+            }}
+        >
+            hello world
+        </div>
+    );
+}
+
+export default App;
