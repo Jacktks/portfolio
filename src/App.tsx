ed50ae6c8dfe59a24fe5378ecaa229b11421befd
Tri Nguyen <tri.nguyen@paradox.ai>
2023-01-04 14:26:53 +0700

    apply dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
+import {useEffect, useState} from 'react';
+import './styles/variables.scss';
 import './App.scss';
+import {ThemeContext} from './context/theme-context';
+import Home from './pages/Home';
 
 function App() {
-    return (
-        <div
-            className="App"
-            style={{
-                background: '#DBDBDB',
-                minWidth: '100vw',
-                minHeight: '100vh',
-            }}
-        >
-            hello world
-        </div>
-    );
+  const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
+
+  useEffect(() => localStorage.setItem('theme', theme), [theme]);
+
+  return (
+    <ThemeContext.Provider value={{theme, setTheme}}>
+      <div
+        className={`App theme--${theme}`}
+        style={{
+          minWidth: '100vw',
+          minHeight: '100vh',
+        }}
+      >
+        <Home />
+      </div>
+    </ThemeContext.Provider>
+  );
 }
 
 export default App;
